a7529db364f9f7a6202757f6c1aac77631ec005b
zhushihao <zhushihao@ybm100.com>
2021-11-10 18:17:46 +0800

    add

--- src/page/layout/index.tsx
+++ src/page/layout/index.tsx
@@ -1,35 +1,34 @@
 import React, { useState, useEffect } from 'react'
 import { StateType, PropsType } from './layoutInterface'
 import SideBar from '@/component/menu'
+import { Layout, Menu, Breadcrumb } from 'antd'
+const { Header, Content } = Layout
 
 import LayoutCss from './index.module.css'
-import { Link } from 'react-router-dom'
 import { renderRoutes } from 'react-router-config'
 
 const LayoutComponent: React.FC<PropsType> = props => {
   const { route } = props
   return (
-    <div className={LayoutCss.box}>
-      {/* <div className={homeCss.navBar}>
-        <div>
-          <Link to="testDemo">demo页</Link>
-        </div>
-        <div>
-          <Link to="login">登录页</Link>
-        </div>
-        <div>
-          <Link to="home">Home页</Link>
-        </div>
-        <div>
-          <Link to="immerDemo">ImmerDemo</Link>
-        </div>
-      </div> */}
+    <Layout style={{ minHeight: '100vh' }}>
       <SideBar />
-      <div className={LayoutCss.content}>
-        <div className={LayoutCss.navBar}>顶部横条</div>
-        {route && renderRoutes(route.routes)}
-      </div>
-    </div>
+      <Layout>
+        <Header className={LayoutCss.header}>
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+            <Menu.Item key="1">nav 1</Menu.Item>
+            <Menu.Item key="2">nav 2</Menu.Item>
+            <Menu.Item key="3">nav 3</Menu.Item>
+          </Menu>
+        </Header>
+        <Content className={LayoutCss.contentHeader}>
+          {/* <Breadcrumb className={LayoutCss.navBar}>
+            <Breadcrumb.Item>User</Breadcrumb.Item>
+            <Breadcrumb.Item>Bill</Breadcrumb.Item>
+          </Breadcrumb> */}
+          <div className={LayoutCss.content}>{route && renderRoutes(route.routes)}</div>
+        </Content>
+      </Layout>
+    </Layout>
   )
 }
 export default LayoutComponent
