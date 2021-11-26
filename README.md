205fd895d14ca34fd14bb57c33564f33c024304a
zhushihao <zhushihao@ybm100.com>
2021-11-26 18:55:36 +0800

    add

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # vite-react-ts
+
+eslint & lint-staged & husky & prettier：代码校验配置
--- src/page/table/table.tsx
+++ src/page/table/table.tsx
@@ -1,7 +1,32 @@
-import React, { useState, useEffect } from 'react'
-import { PropsType } from './tableInterface'
+import React, { useState, useEffect, useRef } from 'react'
+import { PropsType, StateType } from './tableInterface'
+import { Button } from 'antd'
 
 const TableComponent: React.FC<PropsType> = props => {
-  return <div>Table</div>
+  const [number, setNumber] = useState<StateType['number']>(0)
+
+  useEffect(() => {
+    console.log(number, '---页面加载/更新---')
+  }, [number])
+
+  const clickHandle = () => {
+    setNumber(1)
+    setNumber(2)
+  }
+
+  const clickTimer = () => {
+    setTimeout(() => {
+      setNumber(3)
+      setNumber(4)
+    }, 0)
+  }
+
+  return (
+    <div>
+      Table
+      <Button onClick={clickHandle}>{number}</Button>
+      <Button onClick={clickTimer}>Timer</Button>
+    </div>
+  )
 }
 export default TableComponent
